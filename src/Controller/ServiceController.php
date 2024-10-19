aaf095bad7be021c367686dfcecc535155104f59
iyed <iyed.mohamed@isimg.tn>
2024-10-19 23:08:53 +0100

    initial commit

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ServiceController extends AbstractController
+{
+    #[Route('/service', name: 'app_service')]
+    public function index(): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+    //methode show pour prendre en parametre name et l'affiche
+    #[Route("/show/{name}",name:'show_name')]
+    public function show($name){
+        return $this->render('/service/show.html.twig', [
+            'name' => $name
+        ]);
+    }
+    #[Route("/index",name:'go-to-index')]
+    public function goToIndex(){
+        return $this->redirectToRoute('app_service');
+    }
+}
